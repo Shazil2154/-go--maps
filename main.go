27b0d06f7072db296b60f8ad472f9b540455b0fd
Shazil2154 <sattarshazil@gmail.com>
2022-10-31 02:08:40 +0500

    completed task maps

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"white": "#ffffff",
+		"black": "#000000",
+	}
+
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex color for", color, "is", hex)
+	}
 }
